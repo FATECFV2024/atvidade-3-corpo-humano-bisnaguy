ade8d6a78e06c6d7c874dbdcd8aa11ce5f20c70d
Gabriel Azambuja Silva <azambujagabriel95@gmail.com>
2024-09-23 19:03:04 -0300

    [fix] Mudei o nome do objeto para corpo

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,19 +8,19 @@ public class App {
         Scanner escrita = new Scanner(System.in);
         escrita.useLocale(Locale.US);
 
-        CorpoHumano c1 = new CorpoHumano(80,60,50,1.65);
+        CorpoHumano corpo = new CorpoHumano(80,60,50,1.65);
 
-        System.out.println("O corpo tem o volume de "+c1.getVolume()+", tem a massa de "+c1.getMassa()+", tem a densidade de "+c1.getDensidade()+" e por fim tem a altura de "+c1.getAltura());
+        System.out.println("O corpo tem o volume de "+corpo.getVolume()+", tem a massa de "+corpo.getMassa()+", tem a densidade de "+corpo.getDensidade()+" e por fim tem a altura de "+corpo.getAltura());
 
         System.out.println("Quanto você pesa? ");
         double peso = escrita.nextDouble();
-        c1.setMassa(peso);
+        corpo.setMassa(peso);
         
         System.out.println("Qual é a sua altura: ");
         double altura = escrita.nextDouble();
-        c1.setAltura(altura);
+        corpo.setAltura(altura);
 
-        System.out.println("Seu IMC é "+c1.calcularIMC());
+        System.out.println("Seu IMC é "+corpo.calcularIMC());
 
 
         escrita.close();
